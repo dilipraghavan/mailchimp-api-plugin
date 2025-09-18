269855bde4386aef080298c645ad17756103fc85
Dilip Raghavan <dilip.raghavan.cse@gmail.com>
2025-09-18 09:37:13 -0600

    feat: Add Composer autoloader and refactor filenames to PSR-4

--- mailchimp-api-integration.php
+++ mailchimp-api-integration.php
@@ -4,7 +4,7 @@
  * 
  * Plugin Name: Plugin to integrate with Mailchimp
  * Description: Integrate WordPress websites with Mailchimp. Users can subscribe from a shortcode form. Admin can view status from dashboard and export reports. API keys are stored securely through env settings.
- * Version: 0.1.0
+ * Version: 1.0.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Plugin URI: https://github.com/dilipraghavan/mailchimp-api-plugin.git
@@ -14,6 +14,7 @@
  * Text Domain: mailchimp-api-integration
  */
 
+
 namespace MC_API;
 if(! defined('ABSPATH')) exit;
 
@@ -21,21 +22,16 @@ if(! defined('ABSPATH')) exit;
 
 define('MC_API_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__ ));
 define('MC_API_PLUGIN_URL_PATH', plugin_dir_url(__FILE__ ));
-define('MC_API_PLUGIN_VERSION', '0.1.0');
+define('MC_API_PLUGIN_VERSION', '1.0.0');
 define('MC_API_BASENAME', plugin_basename( __FILE__ ));
 define('MC_API_SETTINGS_SLUG', 'mc-api-settings');
 define('MC_API_ACTIONS_SLUG', 'mc-api-test-connection');
 
+require_once(MC_API_PLUGIN_DIR_PATH) . 'vendor/autoload.php';
+
 register_activation_hook( __FILE__, __NAMESPACE__ . '\mc_api_activate');
 register_deactivation_hook( __FILE__, __NAMESPACE__ . '\mc_api_deactivate');
 
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-bootstrap.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-settings.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-subscribe-shortcode.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-mailchimp-client.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-logger.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-rest-subscribe.php');
-include_once(MC_API_PLUGIN_DIR_PATH . '/includes/class-admin-post-subscribe.php');
 
 $mc_api = new MC_API();
 Settings::init();
